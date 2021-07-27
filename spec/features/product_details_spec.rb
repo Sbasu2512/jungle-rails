1bffab57a4f2f076215f526b6b9b3554abb9650f
Sayantan Basu <sayantanbasu@live.in>
2021-07-27 22:14:40 +0000

    product_detail spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # pending "add some scenarios (or delete) #{__FILE__}"
   #! SETUP
   before :each do
@@ -15,5 +15,19 @@ RSpec.feature "ProductDetails", type: :feature do
         price: Faker::Number.decimal(l_digits:2),
       )
     end
+  end
+
+  scenario "They see all products" do
+    #TODO: ACT
+    visit root_path
+    expect(page).to have_css 'article.product', count: 10
+    first("a[href='/products/10']").click
+    sleep 1
+
+    #TODO: DEBUG / VERIFY
+    # save_screenshot
+    #TODO: VERIFY
+    
+    expect(page).to have_css 'article.product-detail'
   end
 end
